97d0c48aeb029db9e330de707c7b5b0db731bb59
unknown <paddymchale@hotmail.com>
2018-08-31 23:13:23 +0100

    initial player

--- Assets/Player/Scripts/PlayerMove.cs
+++ Assets/Player/Scripts/PlayerMove.cs
@@ -8,18 +8,21 @@ public class PlayerMove : MonoBehaviour {
 
     public float direction;
 
-	void Start () {
-		
-	}
+    public string input1;
+    public string input2;
+
+    void Start () {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("a"))
+        if (Input.GetKey(input1))
         {
             Vector3 rotation = new Vector3(0.0f, 0.0f, -direction);
             transform.Rotate(rotation * Time.deltaTime);
         }
-        else if (Input.GetKey("d"))
+        else if (Input.GetKey(input2))
         {
             Vector3 rotation = new Vector3(0.0f, 0.0f, direction);
             transform.Rotate(rotation * Time.deltaTime);
