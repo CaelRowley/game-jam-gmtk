bff9b89d9ff14c2de164c95368cd435b0c4ba208
unknown <paddymchale@hotmail.com>
2018-08-31 22:47:18 +0100

    initail Project setup

---
+++ Assets/Player/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float direction;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("a"))
+        {
+            Vector3 rotation = new Vector3(0.0f, 0.0f, -direction);
+            transform.Rotate(rotation * Time.deltaTime);
+        }
+        else if (Input.GetKey("d"))
+        {
+            Vector3 rotation = new Vector3(0.0f, 0.0f, direction);
+            transform.Rotate(rotation * Time.deltaTime);
+        }
+    }
+}
