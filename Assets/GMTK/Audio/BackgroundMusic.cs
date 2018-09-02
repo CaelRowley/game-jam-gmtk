74e1c1174ac00e172a2f5e2ee95086e5c570e4a4
CaelRowley <caelrowley@gmail.com>
2018-09-02 16:39:01 +0100

    Added background music and vitory audio objects to game

---
+++ Assets/GMTK/Audio/BackgroundMusic.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+    public float audioLength;
+    public AudioClip[] audioClips = new AudioClip[2];
+    public float volume;
+
+    private double nextLoopTime;
+    private int currentIndex = 0;
+    private AudioSource[] audioSources = new AudioSource[2];
+
+    public string tag;
+
+    // Creates audio sources as children to Player
+    private void Start() {
+        if(!GameObject.Find(tag)) {
+            DontDestroyOnLoad(this.gameObject);
+            for(int i = 0; i < 2; i++) {
+                GameObject child = new GameObject(tag);
+                child.transform.parent = gameObject.transform;
+                audioSources[i] = child.AddComponent<AudioSource>();
+                audioSources[i].volume = volume;
+            }
+            nextLoopTime = AudioSettings.dspTime;
+        }
+    }
+
+    // Plays audio clip after the loop time and switches the index for the next audio clip
+    private void Update() {
+        if(audioSources[currentIndex] != null) {
+            double currentTime = AudioSettings.dspTime;
+            if(currentTime > nextLoopTime) {
+                audioSources[currentIndex].clip = audioClips[currentIndex];
+                audioSources[currentIndex].PlayScheduled(nextLoopTime);
+                nextLoopTime += audioLength;
+                currentIndex = 1 - currentIndex;
+            }
+        }
+    }
+}
