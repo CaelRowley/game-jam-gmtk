ae93cf1d7716c9ef586d75f208d7afc5001bf2f1
CaelRowley <caelrowley@gmail.com>
2018-09-02 17:42:35 +0100

    Falling spike trap

---
+++ Assets/fallingTrapMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingTrapMovement : MonoBehaviour {
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, moveSpeed, 0);
+    }
+}
